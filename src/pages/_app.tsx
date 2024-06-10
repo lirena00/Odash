2a0057c78210539786fec13c3f12263f2906d2f8
lirena <sakshamkushwaha12776@gmail.com>
2024-06-11 02:27:33 +0530

    added umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { SettingsProvider } from "@/contexts/SettingsContext";
 import { WidgetsProvider } from "@/contexts/WidgetsContext";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <SettingsProvider>
       <WidgetsProvider>
+        <Head>
+          <script
+            defer
+            src="https://analytics.lirena.in/script.js"
+            data-website-id="40c3bcb0-bc51-4dae-9871-616b5c155aa0"
+          ></script>
+        </Head>
         <Component {...pageProps} />
       </WidgetsProvider>
     </SettingsProvider>
